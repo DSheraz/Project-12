e1b3a97f856d61127e79b4f2e7f1d26c64f502e8
DSheraz <dyg.shahira@mayapersada.com>
2018-05-06 14:38:59 +0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// var header = document.querySelector('header');
+//
+//
+// function design() {
+//   var frameDesign = document.createElement('div');
+//   frameDesign.className = "frameDesign";
+//   header.appendChild(frameDesign);
+//   for(let i = 1; i<5; i+=1) {
+//     var circle = document.createElement('div');
+//     circle.className = "circle" + i;
+//     circle.classList.add('metal');
+//     frameDesign.appendChild(circle);
+//     var line = document.createElement('div');
+//     line.className = "line" + i;
+//     frameDesign.appendChild(line);
+//   }
+//
+// }
+//
+// design();
